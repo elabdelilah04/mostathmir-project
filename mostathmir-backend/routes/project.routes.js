f95f7dada9b64dcd0c6e367bb37fd0c0306e252e
abdelilah <abdilahmoutaouakil@gmail.com>
2025-11-13 17:04:34 +0100

    fix: Preserve file extensions on Cloudinary upload

--- mostathmir-backend/routes/project.routes.js
+++ mostathmir-backend/routes/project.routes.js
@@ -15,6 +15,7 @@ const {
     getInvestmentsInMyProjects
 } = require('../controllers/project.controller.js');
 const { protect, getAuthUser } = require('../middleware/auth.middleware.js');
+const path = require('path'); // تأكد من استيراد 'path'
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -27,12 +28,13 @@ const storage = new CloudinaryStorage({
     params: (req, file) => {
         const resourceType = file.mimetype.startsWith('image') ? 'image' : 'raw';
 
+        const fileName = path.parse(file.originalname).name;
+        const publicId = `${fileName}-${Date.now()}`;
+
         return {
             folder: `mostathmir_projects/${req.user._id}`,
-            allowed_formats: ['jpeg', 'jpg', 'png', 'gif', 'pdf', 'doc', 'docx', 'ppt', 'pptx'],
-            public_id: `${file.fieldname}-${Date.now()}`,
-            resource_type: resourceType // <== هذا هو السطر الحاسم
-
+            public_id: publicId,
+            resource_type: resourceType,
         };
     }
 });
